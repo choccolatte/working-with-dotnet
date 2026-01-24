b75e30580a341cbf4bbc66248167c72c38fcb872
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-01-25 00:19:04 +0530

    mmore methods and adding default parameter to my method - v2501260019

--- methodprac.cs
+++ methodprac.cs
@@ -1,13 +1,21 @@
 class Program
 {
-	static void MyMet()
+	static void Main(string[] args)
+	{
+		MyMet("Jack");
+		Console.WriteLine(MyIntMet(10));
+	}
+
+	static void MyMet(string fname = "John", string lname = "Doe")
 	{
 		// code to execute
 		Console.WriteLine("Hello World!");
+		Console.WriteLine($"Hello {fname} {lname}...");
 	}
 
-	static void Main(string[] args)
+	// returning int
+	static int MyIntMet(int xnum)
 	{
-		MyMet();
+		return (5 * xnum) / 2; // 25
 	}
 }

e3b84c0d0f616c47fb8462ae53e8d7f1d9cecd19
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-01-24 21:13:02 +0530

    methods and dict - v2401262112

--- DictPrac.cs
+++ DictPrac.cs
@@ -1,16 +1,36 @@
-Dictionary<string, int> gradeCount = new Dictionary<string, int>();
-gradeCount["A"] = 0;
-gradeCount["B"] = 0;
-gradeCount["C"] = 0;
+Dictionary<string, int> gradeCount = new Dictionary<string, int>(){
+// initialize with collections {} - modify with indexers - []
+{"A", 0}, // gradeCount["A"] = 0;
+{"B", 0}, // gradeCount["B"] = 0;
+{"C", 0}, // gradeCount["C"] = 0;
+{"D", 0}, // gradeCount["D"] = 0;
+{"F", 0}// gradeCount["F"] = 0;
+};
+string[] grades = {"A", "B", "C", "D", "F"};
 
-string grade = "A";
-
-gradeCount[grade]++;
-gradeCount[grade]++;
-
-Console.WriteLine(gradeCount["A"]); // outputs 2
-
-foreach(var item in gradeCount)
+foreach(string grade in grades)
 {
-	Console.WriteLine(item.key + ":" + item.value);
+	gradeCount[grade]++;
 }
+
+foreach(var entry in gradeCount)
+{
+	Console.WriteLine(entry.key + ":" + entry.value);
+}
+
+if(!gradeCount.ContainsKey("A"))
+{
+	gradeCount["A"]++;
+}
+
+// string grade = "A";
+
+// gradeCount[grade]++;
+// gradeCount[grade]++;
+
+// Console.WriteLine(gradeCount["A"]); // outputs 2
+
+// foreach(var item in gradeCount)
+// {
+// 	Console.WriteLine(item.key + ":" + item.value);
+// }
---
+++ methodprac.cs
@@ -0,0 +1,13 @@
+class Program
+{
+	static void MyMet()
+	{
+		// code to execute
+		Console.WriteLine("Hello World!");
+	}
+
+	static void Main(string[] args)
+	{
+		MyMet();
+	}
+}

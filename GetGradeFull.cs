aabe928b71cab8f64c659922e12e89004715db83
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-01-29 20:38:31 +0530

    updated getGrade method - v2901262038

--- GetGradeFull.cs
+++ GetGradeFull.cs
@@ -18,8 +18,32 @@ class Program
 		return students.Count;
 	}
 
+	// pass/fail counter
+	static int PassStudents(List<int> marks)
+	{
+		int passcount = 0;
+
+		foreach(int mark in marks){
+			if(mark >= 40) passcount++;		
+		}
+
+		return passcount;
+	}
+
+	static int FailStudents(List<int> marks)
+	{
+		int failcount = 0;
+
+		foreach(int mark in marks){
+			if(mark < 40) failcount++;			
+		}
+
+		return failcount;
+	}
+
 	static void Main(string[] args)
 	{
+		
 		Dictionary<string, int> gradeCounter = new Dictionary<string, int>()
         {
             { "A+", 0 },
@@ -36,6 +60,10 @@ class Program
 		};
 
 		marks.Add(99);
+
+		// declaring pass/fail variables
+		int passStudentsNumber = PassStudents(marks);
+		int failStudentsNumber = FailStudents(marks);
 		
 		// int[] marks = {20, 30, 40, 56, 67, 78, 89, 90, 12, 23, 34, 54, 76, 87, 89, 100};
 
@@ -51,6 +79,10 @@ class Program
 		Console.WriteLine("\n---Total Students---");
 		int studentnum = TotalStudents(marks);
 		Console.WriteLine(studentnum);
+
+		Console.WriteLine("\n---Passed/Failed Students---");
+		Console.WriteLine($"Passed Students: {passStudentsNumber}");
+		Console.WriteLine($"Failed Students: {failStudentsNumber}");
 
 		Console.WriteLine("\n-----Grade Summary-----");
 		foreach(var kvp in gradeCounter)

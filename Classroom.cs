5594f64804083b5ef737d28b283d5298634ea46e
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-02-12 14:04:15 +0530

    added a Classroom class - v1202261404

---
+++ Classroom.cs
@@ -0,0 +1,28 @@
+using System.Runtime.CompilerServices;
+
+public class Classroom
+{
+	public List<Student> students;
+
+	public Classroom(List<Student> students)
+	{
+		this.students = students;
+	}
+
+	public double AvgMarks()
+	{
+		students.Average(s => s.Marks);
+	}
+
+	public Student Topper()
+	{
+		students.OrderByDescending(s => s.Marks).First();
+	}
+
+	public Dictionary<Grade, int> GradeSummary()
+	{
+		students.
+		GroupBy(s => s.Grade)
+		.ToDictionary(g => g.Key, g => g.Count());
+	}
+}
